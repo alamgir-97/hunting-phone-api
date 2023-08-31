7a791ef01caf938f70ce4174abed7e54c08dc3bf
Alamgir <agir97@gmail.com>
2023-08-31 13:41:08 +0600

    added search functionality

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-const loadPhone = async () => { 
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText=13) => { 
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
+    //see the search output length
+    const dataLength = phones.length;
+    console.log(dataLength);
     //console.log(phones);
     displayPhones(phones);
 }
 const displayPhones = phones => {
     const phoneContainer = document.getElementById('phone-container');
+    //clear phone container before search
+    phoneContainer.innerText = "";
     //console.log(phoneContainer);
     phones.forEach(phone => {
         console.log(phone); 
         const phoneCard = document.createElement('div');
-        phoneCard.classList = "card bg-base-100 shadow-xl my-4 ";
+        phoneCard.classList = "card bg-base-100 shadow-xl my-4 p-2";
         phoneCard.innerHTML = `<figure><img src="${phone.image}" alt="Phone" /></figure>
-        < class="card-body">
+        <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
 
@@ -36,11 +41,16 @@ const displayPhones = phones => {
             </div>
         </div>`        
         phoneContainer.appendChild(phoneCard);
-    });
-
-    
+    });    
 }
 
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+//loadPhone();
 
 
 
@@ -55,6 +65,5 @@ const displayPhones = phones => {
 
 
 
-loadPhone();
 
 
